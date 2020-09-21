5138957c12a931ef92cb3b5f918b7d60ad3aee5c
Adorevoy <47337588+jae04099@users.noreply.github.com>
2020-09-21 19:48:02 +0900

    Add loading animation with material UI

--- client/src/App.js
+++ client/src/App.js
@@ -7,36 +7,46 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { withStyles } from "@material-ui/core/styles";
 
-const styles = theme => ({
+const styles = (theme) => ({
     root: {
-        width: '100%',
+        width: "100%",
         marginTop: theme.spacing.unit * 3,
-        overflowX: 'auto'
+        overflowX: "auto",
     },
     table: {
-        minWidth: 1080
-    }
+        minWidth: 1080,
+    },
+    progress: {
+        margin: theme.spacing.unit * 2,
+    },
 });
 
 class App extends Component {
-
     state = {
-        customers: ""
-    }
+        customers: "",
+        completed: 0,
+    };
 
-    componentDidMount(){
+    componentDidMount() {
+        this.timer = setInterval(this.progress, 20);
         this.callApi()
-        .then(res => this.setState({customers: res}))
-        .catch(err => console.log(err));
+            .then((res) => this.setState({ customers: res }))
+            .catch((err) => console.log(err));
     }
 
-callApi = async () => {
-    const response = await fetch('/api/customers');
-    const body = await response.json();
-    return body;
-}
+    callApi = async () => {
+        const response = await fetch("/api/customers");
+        const body = await response.json();
+        return body;
+    };
+
+    progress = () => {
+        const { completed } = this.state;
+        this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+    };
 
     render() {
         const { classes } = this.props;
@@ -54,19 +64,25 @@ callApi = async () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {this.state.customers ? this.state.customers.map((c) => {
-                            return (
-                                <Customer
-                                    key={c.id}
-                                    id={c.id}
-                                    image={c.image}
-                                    name={c.name}
-                                    birth={c.birth}
-                                    gender={c.gender}
-                                    job={c.job}
-                                />
-                            );
-                        }): ''}
+                        {this.state.customers
+                            ? this.state.customers.map((c) => {
+                                  return (
+                                      <Customer
+                                          key={c.id}
+                                          id={c.id}
+                                          image={c.image}
+                                          name={c.name}
+                                          birth={c.birth}
+                                          gender={c.gender}
+                                          job={c.job}
+                                      />
+                                  );
+                              })
+                            : <TableRow>
+                                <TableCell colSpan="6" align="center">
+                                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                                </TableCell>
+                                </TableRow>}
                     </TableBody>
                 </Table>
             </Paper>
