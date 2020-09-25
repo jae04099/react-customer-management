68ca656669fdb8240c8693d84e52a421a6b8d0d0
Adorevoy <47337588+jae04099@users.noreply.github.com>
2020-09-25 17:17:59 +0900

    Implement customer add module, refresh module

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Customer from "./components/Customer";
+import CustomerAdd from "./components/CustomerAdd";
 import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
@@ -25,10 +26,24 @@ const styles = (theme) => ({
 });
 
 class App extends Component {
-    state = {
-        customers: "",
-        completed: 0,
-    };
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            cusstomers: '',
+            completed: 0
+        }
+    }
+
+    stateRefresh = () => {
+        this.setState({
+            customers: '',
+            completed: 0
+        });
+        this.callApi()
+            .then((res) => this.setState({ customers: res }))
+            .catch((err) => console.log(err));
+    }
 
     componentDidMount() {
         this.timer = setInterval(this.progress, 20);
@@ -51,6 +66,7 @@ class App extends Component {
     render() {
         const { classes } = this.props;
         return (
+            <div>
             <Paper className={classes.root}>
                 <Table className={classes.table}>
                     <TableHead>
@@ -86,6 +102,8 @@ class App extends Component {
                     </TableBody>
                 </Table>
             </Paper>
+            <CustomerAdd stateRefresh={this.stateRefresh}/>
+            </div>
         );
     }
 }
