9fcc4129866f7120ed4d4f37a93dfe9a3f94775b
Adorevoy <47337588+jae04099@users.noreply.github.com>
2020-09-29 17:19:43 +0900

    Add Material UI Design for Add/Delete

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -7,35 +7,35 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const data = fs.readFileSync('./database.json');
+const data = fs.readFileSync("./database.json");
 const conf = JSON.parse(data);
-const mysql = require('mysql');
-const { cpuUsage } = require('process');
+const mysql = require("mysql");
+const { cpuUsage } = require("process");
 
 const connection = mysql.createConnection({
     host: conf.host,
     user: conf.user,
     password: conf.password,
     port: conf.port,
-    database: conf.database
+    database: conf.database,
 });
 connection.connect();
 
-const multer = require('multer');
-const upload = multer({dest: './upload'})
+const multer = require("multer");
+const upload = multer({ dest: "./upload" });
 
-app.get('/api/customers', (req, res) => {
+app.get("/api/customers", (req, res) => {
     connection.query(
-        "SELECT * FROM CUSTOMER",
+        "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
         (err, rows, fields) => {
             res.send(rows);
         }
     );
 });
-app.use('/image', express.static('./upload'))
-app.post('/api/customers', upload.single('image'), (req,res) => {
-    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ? )'
-    let image = '/image/' + req.file.filename;
+app.use("/image", express.static("./upload"));
+app.post("/api/customers", upload.single("image"), (req, res) => {
+    let sql = "INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(), 0)";
+    let image = "/image/" + req.file.filename;
     let name = req.body.name;
     let birth = req.body.birth;
     let gender = req.body.gender;
@@ -43,7 +43,15 @@ app.post('/api/customers', upload.single('image'), (req,res) => {
     let params = [image, name, birth, gender, job];
     connection.query(sql, params, (err, rows, fields) => {
         res.send(rows);
-    })
-})
+    });
+});
+
+app.delete("/api/customers/:id", (req, res) => {
+    let sql = "UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?";
+    let params = [req.params.id];
+    connection.query(sql, params, (err, rows, fields) => {
+        res.send(rows);
+    });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
